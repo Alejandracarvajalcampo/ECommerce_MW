55dd048a5b9560bf852ecfa8a78e43f8fcc795a6
AlejandraCarvajalCampo <alejandracarvajal316210@correo.itm.edu.co>
2023-03-26 18:51:49 -0500

    New Categories table created

---
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce_MW.DAL.Entities
+{
+    public class Category: Entity
+    {
+        [Display(Name = "Categoria")]
+        [MaxLength(50, ErrorMessage = " El campo {0} deber ser {1} caracteres")]
+        [Required(ErrorMessage = " El campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
